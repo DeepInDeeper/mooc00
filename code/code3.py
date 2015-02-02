0ad511a3f4ce15db166e6581eb28830e85f8ce63
peterchen <2232941357@qq.com>
2015-02-02 09:56:41 +0800

    add code3

---
+++ code/code3.py
@@ -0,0 +1,10 @@
+username = raw_input ("what is your name?  ")
+print ("are you "+ username+"?")
+answer = raw_input ("YES OR NO?   ")
+if answer == "YES":
+    print ("WECLOME TO STUDY PYTHON")
+else :
+    print ("please correct")
+
+    
+
