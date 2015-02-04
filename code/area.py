c24172cc408f3a76251bc67011b0760b746d1629
peterchen <2232941357@qq.com>
2015-02-04 15:00:40 +0800

    add lec4

---
+++ code/area.py
@@ -0,0 +1,17 @@
+# calucate area
+
+pi = 3.14159
+
+def Circle_area (r):
+    return pi *(r**2)
+
+def Ball_area (r):
+    return 4 * pi * (r**2)
+
+def Rectangle_area (a,b):
+    return a * b
+
+def Square_area (a):
+    return a * a
+def Trapezoid_area (top,bottom,high):
+    return (top + bottom) * high /2.0
