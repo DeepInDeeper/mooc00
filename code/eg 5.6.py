66fcf492c669d341d77190fe19986ceea677b7db
peterchen <2232941357@qq.com>
2015-02-09 16:19:19 +0800

    add code and ppt in lec5

---
+++ code/eg 5.6.py
@@ -0,0 +1,11 @@
+def lenIter(aStr):
+    '''
+    aStr: a string
+    
+    returns: int, the length of aStr
+    '''
+    # Your code here
+    n = 0
+    for char in aStr:
+        n += 1
+    return n
