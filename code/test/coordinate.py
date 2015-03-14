14cd9d9416963d4e0bbac0318b68bc0745257a11
peterchen <2232941357@qq.com>
2015-03-14 09:01:43 +0800

    add Lec12

--- code/test/coordinate.py
+++
@@ -1,24 +0,0 @@
-import math
-
-def sq(x):
-    return x*x
-
-
-class Coordinate(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    def __str__(self):
-        return "<"+str(self.x)+","+str(self.y)+">"
-    def distance(self,other):
-        return math.sqrt(sq(self.x - other.x)
-                         + sq(self.y - other.y))
-
-c = Coordinate(3,4)
-Origin = Coordinate(0,0)
-print c.x,Origin.x
-print c.distance(Origin)
-print c.__str__()
-c.__init__(5,6)
-print c.x
-print isinstance (c,Coordinate)
