28d193fcd65e3a655073932c8d513afde8ebc97e
peterchenyijie <2232941357@qq.com>
2016-10-27 23:17:57 +0800

    start code-0.2

---
+++ code/eg/eg 1.1.py
@@ -0,0 +1,8 @@
+# test "while"
+num = 10
+print "Hello!"
+while num > 1:
+    print str(num)
+    num -=  2   
+
+    
--- code/eg/eg1.1.py
+++
@@ -1,8 +0,0 @@
-# test "while"
-num = 10
-print "Hello!"
-while num > 1:
-    print str(num)
-    num -=  2   
-
-    
